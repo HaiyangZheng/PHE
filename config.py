ce8f16f3a2597161499162b1711a426758eefca3
郑海阳 <zhy@zhenghaiyangdeMacBook-Air.local>
2024-12-09 14:09:33 +0100

    Initial commit

--- config.py
+++ config.py
@@ -1,15 +1,13 @@
 # --------------------
 # DATASETS ROOT
 # --------------------  
-cub_root = '/leonardo_work/IscrC_Fed-GCD/GCD_datasets/CUB'
-# cub_root = '/home/fandral/Nan/NCD_dataset/CUB'
-car_root = '/leonardo_work/IscrC_Fed-GCD/GCD_datasets/stanford_cars'
-food_101_root = '/leonardo_work/IscrC_Fed-GCD/GCD_datasets'
-oxford_pet_root = '/leonardo_work/IscrC_Fed-GCD/GCD_datasets'
-inaturalist_root = '/leonardo_work/IscrC_Fed-GCD/GCD_datasets/iNaturalist'
+cub_root = '/home/hyzheng/GCD_datasets/CUB'
+car_root = '/home/hyzheng/GCD_datasets/stanford_cars'
+food_101_root = '/home/hyzheng/GCD_datasets'
+oxford_pet_root = '/home/hyzheng/GCD_datasets'
+inaturalist_root = '/home/hyzheng/GCD_datasets/iNaturalist'
 
 # --------------------
 # DINO PRETRAIN ROOT
 # --------------------  
-pretrain_path = '/leonardo_work/IscrC_Fed-GCD/GCD_datasets/dino/dino_vitbase16_pretrain.pth'
-# pretrain_path = '/home/fandral/.cache/torch/hub/checkpoints/dino_vitbase16_pretrain.pth'
+pretrain_path = '/home/hyzheng/GCD_datasets/dino/dino_vitbase16_pretrain.pth'
